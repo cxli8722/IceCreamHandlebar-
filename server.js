88aa05bb317b8bec13c97f719a2255fe8785d574
Li Chen <cxli@email.unc.edu>
2018-05-13 10:23:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// Dependencies
+
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
+
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
+
 var PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -23,7 +22,7 @@ var icecreams = [
   { name: "vanilla", price: 10, awesomeness: 3 }
 ];
 
-// Routes
+
 app.get("/icecream/:name", function(req, res) {
   for (var i = 0; i < icecreams.length; i++) {
     if (icecreams[i].name === req.params.name) {
@@ -36,8 +35,7 @@ app.get("/icecreams", function(req, res) {
   res.render("ics", { ics: icecreams });
 });
 
-// Start our server so that it can begin listening to client requests.
+
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
